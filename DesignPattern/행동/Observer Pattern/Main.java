37d170243bcbcf8fb085137173a253b63e936ca1
jiyumi <jiyumi00@naver.com>
2024-05-21 15:38:49 +0900

    옵저버 패턴

---
+++ DesignPattern/행동/Observer Pattern/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+	public static void main(String[]args){
+		Topic topic=new Topic();
+		
+		Observer a=new TopicSubscriber("a",topic);
+		Observer b=new TopicSubscriber("b",topic);
+		Observer c=new TopicSubscriber("c",topic);
+		topic.register(a);
+		topic.register(b);
+		topic.register(c);
+		
+		topic.postMessage("amuamu is op champion!!");
+	}
+}
